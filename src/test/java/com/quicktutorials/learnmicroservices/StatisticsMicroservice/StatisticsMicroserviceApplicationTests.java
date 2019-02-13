2b00dbc0f41278d8cde20c645a6e9341c390a998
durga <2017durga@gmail.com>
2019-02-12 22:29:03 -0600

    first commit

---
+++ src/test/java/com/quicktutorials/learnmicroservices/StatisticsMicroservice/StatisticsMicroserviceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.quicktutorials.learnmicroservices.StatisticsMicroservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StatisticsMicroserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
